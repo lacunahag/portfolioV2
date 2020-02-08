cdd1917c9567aef962df1abd318866ab4a592f85
Jasmine Humbert <jasmine@humbert.dev>
2020-02-07 16:45:10 -0800

    Deploy

--- README.md
+++
@@ -1,1 +0,0 @@
-My portfolio. It's a major work in progress so check back later.
---
+++ precache-manifest.9b58e87db5445746e0269ab167cdc5b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0bd989df055df530c8de0ec35885be0",
+    "url": "/portfolioV2/index.html"
+  },
+  {
+    "revision": "5810fd71d008ede82766",
+    "url": "/portfolioV2/static/css/main.515fa2a5.chunk.css"
+  },
+  {
+    "revision": "6782e1c9dee9c58f0e88",
+    "url": "/portfolioV2/static/js/2.fda4e1dd.chunk.js"
+  },
+  {
+    "revision": "27f24f313e7751d4c8f1d7aa23e3ac15",
+    "url": "/portfolioV2/static/js/2.fda4e1dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5810fd71d008ede82766",
+    "url": "/portfolioV2/static/js/main.0b52343e.chunk.js"
+  },
+  {
+    "revision": "f193d81295f255adacb3",
+    "url": "/portfolioV2/static/js/runtime-main.243b11af.js"
+  }
+]);
